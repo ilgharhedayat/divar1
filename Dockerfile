4107e861c6713c8f16fa1c8612433ee66e102f72
elyas <elyashedayat1380@gmail.com>
2022-07-09 13:54:37 +0430

    ready for production

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM hub.hamdocker.ir/library/python:3.8
 WORKDIR /divar/
-ADD ./requirements.txt ./
-RUN pip install -r ./requirements.txt
+COPY requirements.txt /requirements/base.txt
+RUN pip install -r /requirements/base.txt
 ADD ./ ./
 ENTRYPOINT ["/bin/sh", "-c" , "python manage.py migrate && gunicorn --bind 0.0.0.0:8000 divar.wsgi"]
--- divar/settings/production.py
+++ divar/settings/production.py
@@ -2,10 +2,11 @@ SECRET_KEY = "django-insecure-vvq-h(a-3bqlzp^1f+)#zcxq7d590)pzi$@#9f@q%a+^i7@m#(
 DEBUG = False
 DATABASES = {
     "default": {
-        "ENGINE": "",
-        "NAME": "",
-        "USER": "",
-        "PASSWORD": "",
-        "HOST": "",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "ZnQhsP1JWGrzp3WCkmpcU9lLxiLlrmQq",
+        "HOST": "divardb.elyashedayat10.svc",
+        "PORT": "5432",
     }
 }
