508042f548ca7f24b055f18d54f377af42b2da35
elyas <elyashedayat1380@gmail.com>
2022-07-09 14:03:36 +0430

    update setting file

--- divar/settings/production.py
+++ divar/settings/production.py
@@ -1,5 +1,6 @@
+from .base import *
 SECRET_KEY = "django-insecure-vvq-h(a-3bqlzp^1f+)#zcxq7d590)pzi$@#9f@q%a+^i7@m#("
-DEBUG = False
+DEBUG = True
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
